73300006e53312ecc94d9f3c4e36dc221e9786b7
Jimmy Liu <jliu5@woolworths.com.au>
2020-01-25 11:07:10 +1100

    vagrant

---
+++ threads.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "thread.h"
+
+static void go(int n);
+
+#define NTHREADS 10
+
+static thread_t threads[NTHREADS];
+
+int main(int argc, char **argv) {
+    int i;
+    long exitValue;
+
+    for (i = 0; i < NTHREADS; i++) {
+        thread_create(&(threads[i]), &go, i);
+    }
+    for (i = 0; i < NTHREADS; i++) {
+        exitValue = thread_join(threads[i]);
+        printf("Thread %d returned with %ld\n", i, exitValue);
+    }
+    printf("Main thread done.\n")
+    return 1;
+}
+
+void go(int n) {
+    printf("Hello from thread #%d\n", n);
+    thread_exit(100 + n);
+}
